c1b0f927addb56a0c88d5565c5d53d27079c64a8
Roecker <12001000175299@FEDIDCARD.GOV>
2022-02-25 17:12:34 -0600

    Update s4ss.R

--- s4ss.R
+++ s4ss.R
@@ -9,14 +9,14 @@ setwd("D:/geodata/project_data/gsp-bs/data")
 bs  <- readRDS(file = "tdata_bs.rds")
 bs2 <- subset(bs, is.na(rcasiteid) & !is.na(pedlabsampnum))
 bs2$rcasiteid <- NULL
-names(bs2)[names(bs2) %in% c("BS1", "BS2")] <- c("bs1_obs", "bs2_obs")
+names(bs2)[names(bs2) %in% c("BS1", "BS2")] <- c("BS1_obs", "BS2_obs")
 
 vars <- c(dsm_bs1 = "bs1_dsm_mean.tif",
           dsm_bs2 = "bs2_dsm_mean.tif"
 )
 fp <- "D:/geodata/project_data/gsp-bs"
 rs_bs <- stack(file.path(fp, vars))
-names(rs_bs) <- c("bs1_pred", "bs2_pred")
+names(rs_bs) <- c("BS1_pred", "BS2_pred")
 
 pred <- extract(rs_bs, st_coordinates(bs2))
 bs3 <- cbind(st_coordinates(bs2), bs2, pred)
